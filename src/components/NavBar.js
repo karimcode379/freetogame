2bd79eba1052299c81bba4c5eeb1532df8faac37
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-06-20 17:58:20 +0200

    nav fast fertig

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,50 @@
+import vector from '../img/Vector 1.svg';
+import games from '../img/Games.png';
+import plus from '../img/Union.png';
+import { useState } from 'react';
+
+
+
 const NavBar = () => {
+
+    const [slide, setSlide] = useState(true);
+    let menu = () => {
+        setSlide(false);
+    }
+
+    let notMenu = () => {
+        setSlide(true);
+    }
     return (
-        <nav>
-            <section>
-                <div></div>
-                <div></div>
-                <div></div>
+        <div className="relativeParent">
+            <nav className="flexColumn">
+                <section onClick={menu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </section>
+                <div className="flexColumn">
+                    <img className="navImg" src={vector} alt="house" />
+                    <img className="navImg navGamepad" src={games} alt="gamepad" />
+                    <img className="navImg" src={plus} alt="plus" />
+                </div>
+            </nav>
+
+            {/* className={slide ? "noSlideMenu" : "slideMenu"} */}
+            <section className={slide ? "noSlideMenu" : "slideMenu"}>
+                <section>
+                    <div className="rightX" onClick={notMenu}>x</div>
+                </section>
+                <div className="flexColumnHide">
+                    <a className="aTag flexRow " href="#"><img className="navImg smallImg" src={vector} alt=" house" />Home</a>
+                    <a className="aTag flexRow" href="#"><img className="navImg navGamepad" src={games} alt=" gamepad" />All Games</a>
+                    <a className="aTag flexRow" href="#"><img className="navImg smallImg" src={plus} alt=" plus" />Recently Added</a>
+                </div>
+
             </section>
-        </nav>
+
+        </div >
+
     );
 }
 
