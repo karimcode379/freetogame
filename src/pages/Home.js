99460559d55259edd0ba8cd8b7d1efc2eb112eb7
StephanPagel <stephan.pagel84@gmail.com>
2022-06-20 15:28:18 +0200

    routing links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
-import GameList from './../components/GameList';
+import GameList from "./../components/GameList";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-
-        </div>
-    );
-}
+  return (
+    <div>
+      <Link to="/recently">SHOW MORE</Link>
+      <Link to="/all">SHOW MORE</Link>
+      <Link to="/all">SHOW MORE</Link>
+    </div>
+  );
+};
 
 export default Home;
