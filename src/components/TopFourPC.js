4745dff17cf6720b620c4e125fe94f7a7b03dd1e
karimcode379 <karimcode379@gmail.com>
2022-06-22 17:11:59 +0200

    change screenshot front page

--- src/components/TopFourPC.js
+++ src/components/TopFourPC.js
@@ -1,17 +1,50 @@
 import GameItem_Top4PC from './GameItem_Top4PC';
+import React, { useState, useEffect } from 'react';
 
 const TopFourPC = (props) => {
+
+    const [data2, setData] = useState(null);
+
+
 
     const data = props.data;
     let one = data.filter(obj => obj.id == 475);
     let two = data.filter(obj => obj.id == 516);
     let three = data.filter(obj => obj.id == 57);
     let four = data.filter(obj => obj.id == 1);
-    const topFourPC = one.concat(two).concat(three).concat(four);
+    const topFourPC = two.concat(three).concat(four);
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+                'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+            }
+        };
+        const getDataHandler = () => {
+            fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=475`, options)
+                .then(response => response.json())
+                .then(data2 => {
+                    setData(data2)
+                })
+                .catch(err => console.error(err));
+        }
+        getDataHandler();
+    }, [])
 
     return (
         <main className="top4PCGrid">
-            {topFourPC.map(elt =>
+            {data2 && <GameItem_Top4PC
+                key={one.id}
+                id={one.id}
+                thumbnail={data2.screenshots[0].image}
+                title={one.title}
+                short_description={one.short_description}
+                platform={one.platform}
+                genre={one.genre}
+            />}
+            {data && topFourPC.map(elt =>
                 <GameItem_Top4PC
                     key={elt.id}
                     id={elt.id}
