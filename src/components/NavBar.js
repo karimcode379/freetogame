2ec659324ecfed7222400f0924d0fc8b8b5789b9
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-06-23 18:23:59 +0200

    save before merge

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,84 @@
 import vector from '../img/Home.png';
 import games from '../img/Games.png';
 import plus from '../img/plus.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { NavLink } from "react-router-dom";
 import redlog from '../img/logo.png';
 import freetogame from '../img/Free2Game.png';
+import GameItem from './GameItem';
 
 const NavBar = () => {
     const [slide, setSlide] = useState(true);
     const [topWidth, setTopWidth] = useState("95vw");
+
+    const [input, setInput] = useState('');
+    const [data, setData] = useState([]);
+    const [searchPara] = useState(['title', 'publisher', 'developer', 'genre', 'platform']);
+    const [filterParam, setFilterParam] = useState('');
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+                'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+            }
+        };
+        const fetchApi = () => {
+            fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
+                .then(response => response.json())
+                .then(json => {
+                    setData(json);
+                })
+        }
+        fetchApi();
+    }, []);
+
+    // el.text.toLowerCase().includes(input)
+
+    const filterSearch = (games) => {
+        return games.filter((item) => {
+            if (item.title.toLowerCase().includes(filterParam.toLowerCase())) {
+                console.log(item.title);
+                return searchPara.some((newItem) => {
+                    console.log(item.title, item.developer, item.genre, item.release_date, item.publisher, item.platform);
+                    return (
+                        item[newItem].toString().toLowerCase().indexOf(input.toLowerCase()) > -1
+                    );
+                });
+            } else if (item.publisher == filterParam) {
+                return searchPara.some((newItem) => {
+                    return (
+                        item[newItem].toString().toLowerCase().indexOf(input.toLowerCase()) > -1
+                    );
+                });
+            } else if (item.developer == filterParam) {
+                return searchPara.some((newItem) => {
+                    console.log(newItem);
+                    return (
+                        item[newItem].toString().toLowerCase().indexOf(input.toLowerCase()) > -1
+                    );
+                });
+            } else if (item.genre == filterParam) {
+                console.log(item.genre)
+                return searchPara.some((newItem) => {
+                    return (
+                        item[newItem].toString().toLowerCase().indexOf(input.toLowerCase()) > -1
+                    );
+                });
+            } else if (item.platform == filterParam) {
+                return searchPara.some((newItem) => {
+                    return (
+                        item[newItem].toString().toLowerCase().indexOf(input.toLowerCase()) > -1
+                    );
+                });
+            }
+
+        });
+    }
+    // console.log(input);
+
+
     let menu = () => {
         setSlide(false);
         setTopWidth(false);
@@ -17,6 +87,15 @@ const NavBar = () => {
         setSlide(true);
         setTopWidth(true);
     }
+
+    const getInput = (ele) => {
+        setInput(ele.target.value);
+        setFilterParam(ele.target.value);
+    }
+
+
+
+
     return (
         <section className="Navigation">
             <section style={{ width: topWidth ? "95vw" : "83vw" }}>
@@ -25,7 +104,7 @@ const NavBar = () => {
                     <img src={freetogame} alt="freetogame-letters" />
                 </div>
                 <form action="" className="form">
-                    <input type="text" name="" id="search" />
+                    <input type="text" name="" id="search" onChange={getInput} value={input} />
                 </form>
             </section>
             <div className=" nav relativeParent">
@@ -59,6 +138,20 @@ const NavBar = () => {
                     </div>
                 </section>
             </div >
+            <ul className="searchSearch">
+                {filterSearch(data).map((ele, i) => {
+                    <GameItem
+                        key={i}
+                        id={ele.id}
+                        thumbnail={ele.thumbnail}
+                        title={ele.title}
+                        short_description={ele.short_description}
+                        platform={ele.platform}
+                        genre={ele.genre}
+                        data={data}
+                    />
+                })}
+            </ul>
         </section >
 
     );
